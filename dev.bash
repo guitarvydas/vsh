3127f77c96656d0282a29f702e36c67246113c84
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-24 18:19:41 -0400

    nbsp fixed ; was &amp;nbsp;

--- dev.bash
+++ dev.bash
@@ -1,25 +1,30 @@
 #!/bin/bash
 # node test.js
-node nbsp.js
-pfr nbsp.txt nbsp.ohm nbsp.action
-# ./transpile.bash >7.json
-# names=`node emitBash.js`
-# # echo chmod a+x ${names}
-# chmod a+x ${names}
 
-# mydir=`pwd`
-# # pf - unhtml.ohm <run-queries
+# node nbsp.js
+# pfr nbsp.txt nbsp.ohm nbsp.action
+# pfr nbsp.txt unhtml.ohm unhtml.action
+# pfr create-rect-facts unhtml.ohm unhtml.action
+# pfr create-rect-facts nbsp.ohm nbsp.action
 
-# # pfr run-queries unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
-# # pfr create-rect-facts unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
+./transpile.bash >7.json
+names=`node emitBash.js`
+# echo chmod a+x ${names}
+chmod a+x ${names}
 
-# pfr create-rect-facts unhtml.ohm unhtml.action >temp1
-# cat temp0 | pfr - span.ohm span.action >temp2
-# cat temp2
+mydir=`pwd`
+# pf - unhtml.ohm <run-queries
 
-# #create-rect-facts
+# pfr run-queries unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
+# pfr create-rect-facts unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
 
-# # for i in ${names}
-# # do
-# #     cat $i
-# # done >_all.html
+pfr create-rect-facts unhtml.ohm unhtml.action >temp1
+cat temp1 | pfr - span.ohm span.action >temp2
+cat temp2
+
+#create-rect-facts
+
+# for i in ${names}
+# do
+#     cat $i
+# done >_all.html
