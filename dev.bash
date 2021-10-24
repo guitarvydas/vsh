3e603d3a5b241d727daa2ebc13103e359d29719a
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-24 10:17:56 -0400

    wip: up to para.action

--- dev.bash
+++ dev.bash
@@ -7,10 +7,9 @@ chmod a+x ${names}
 
 mydir=`pwd`
 # pf run-queries unhtml.ohm
-pfr run-queries unhtml.ohm identity-unhtml.action
-
-# pfr temp1 span.ohm span.action >temp2
-# pfr temp2 para.ohm para.action
+pfr run-queries unhtml.ohm unhtml.action >temp1
+pfr temp1 span.ohm span.action >temp2
+pfr temp2 para.ohm para.action
 
 #create-rect-facts
 
