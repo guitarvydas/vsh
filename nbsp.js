1aae3f2b79c3d2dc96ac18a057dd26b85d3f0d3b
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-24 12:40:12 -0400

    add nbsp operation

--- nbsp.js
+++ nbsp.js
@@ -3,7 +3,6 @@ unhtml {
   text = htmlchar*
   htmlchar =
       "&nbsp; " -- nbsp
-    | "&amp;"  -- amp
     | any      -- any
 }
 `;
@@ -13,6 +12,14 @@ var str = `
 # create rect facts
 <p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## create rect fact for every vertex that is not an edge/ellipse/text</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## sequence.drawio file contains vertexes, and marks all edge and ellipse (and text)</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## but does not mark rectangles (the default)</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## this pass finds the defaults and creates explicit rect(...) facts</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>swipl -q &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'consult(5).' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'consult(rects).' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'printRects.' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'halt.' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>> 6.pl</span></p><p class=&quot;p2&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34; ; min-height: 13px&quot;><span class=&quot;s1&quot;></span>echo done >/dev/fd/4</p>
 `;
+
+var nbsp_semantics =
+    {
+	text : function (cs) { return cs.nbsp ().join (''); },
+	htmlchar_nbsp : function (k) { return ' '; },
+	htmlchar_any : function (c) { return c; },
+	_terminal : function () { return this.primitiveValue; }
+    };
 
 
 var ohm = require ('ohm-js');
@@ -24,6 +31,8 @@ var cst = parser.match (str);
 
 if (cst.succeeded ()) {
     console.log ('OK');
+    var sem = parser.createSemantics ();
+    sem.addOperation ('nbsp', nbsp_semantics);
 } else {
     var pos = cst._rightmostFailurePosition;
     console.error (parser.trace (grammar).toString ());
