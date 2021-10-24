45cb20a5d71df93e9b0cce5d058650c58a7ad623
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-24 18:02:43 -0400

    wip: testing nbsp

--- dev.bash
+++ dev.bash
@@ -1,23 +1,25 @@
 #!/bin/bash
 # node test.js
-# node nbsp.js
-./transpile.bash >7.json
-names=`node emitBash.js`
-# echo chmod a+x ${names}
-chmod a+x ${names}
+node nbsp.js
+pfr nbsp.txt nbsp.ohm nbsp.action
+# ./transpile.bash >7.json
+# names=`node emitBash.js`
+# # echo chmod a+x ${names}
+# chmod a+x ${names}
 
-mydir=`pwd`
-# pf - unhtml.ohm <run-queries
+# mydir=`pwd`
+# # pf - unhtml.ohm <run-queries
 
-# pfr run-queries unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
-# pfr create-rect-facts unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
+# # pfr run-queries unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
+# # pfr create-rect-facts unhtml.ohm unhtml.action | pfr - span.ohm span.action | pfr - para.ohm para.action
 
-pfr create-rect-facts unhtml.ohm unhtml.action >temp1
-cat temp1 | pfr - span.ohm span.action >temp2
+# pfr create-rect-facts unhtml.ohm unhtml.action >temp1
+# cat temp0 | pfr - span.ohm span.action >temp2
+# cat temp2
 
-#create-rect-facts
+# #create-rect-facts
 
-# for i in ${names}
-# do
-#     cat $i
-# done >_all.html
+# # for i in ${names}
+# # do
+# #     cat $i
+# # done >_all.html
--- nbsp.js
+++ nbsp.js
@@ -7,7 +7,8 @@ unhtml {
 }
 `;
 
-var str = `
+var str_small = `/abc&nbsp; def/`;
+var str_big = `
 #!/bin/bash
 # create rect facts
 <p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## create rect fact for every vertex that is not an edge/ellipse/text</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## sequence.drawio file contains vertexes, and marks all edge and ellipse (and text)</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## but does not mark rectangles (the default)</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>## this pass finds the defaults and creates explicit rect(...) facts</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;>swipl -q &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'consult(5).' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'consult(rects).' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'printRects.' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>-g 'halt.' &#92;</span></p><p class=&quot;p1&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34;&quot;><span class=&quot;s1&quot;><span class=&quot;Apple-converted-space&quot;>&nbsp; &nbsp; &nbsp; </span>> 6.pl</span></p><p class=&quot;p2&quot; style=&quot;margin: 0px ; font-stretch: normal ; font-size: 11px ; line-height: normal ; font-family: &#34;menlo&#34; ; min-height: 13px&quot;><span class=&quot;s1&quot;></span>echo done >/dev/fd/4</p>
@@ -19,24 +20,42 @@ var nbsp_semantics =
 	htmlchar_nbsp : function (k) { return ' '; },
 	htmlchar_any : function (c) { return c.nbsp (); },
 	_iter : function (...children) { return children.map (c => c.nbsp ()); },
-	_terminal : function () { return this.primitiveValue; }
+	_terminal : function () { return this.sourceString; }
     };
 
 
 var ohm = require ('ohm-js');
 var fs = require ('fs');
 
-var parser = ohm.grammar (grammar);
 
-var cst = parser.match (str);
-
-if (cst.succeeded ()) {
-    console.log ('OK');
-    var sem = parser.createSemantics ();
-    sem.addOperation ('nbsp', nbsp_semantics);
-    console.error (sem (cst).nbsp ());
-} else {
-    var pos = cst._rightmostFailurePosition;
-    console.error (parser.trace (grammar).toString ());
-    console.log ("FAIL: at position " + pos.toString ());
+function test (grammar, str) {
+    var parser = ohm.grammar (grammar);
+    var cst = parser.match (str);
+    
+    if (cst.succeeded ()) {
+	console.log ('OK');
+	var sem = parser.createSemantics ();
+	sem.addOperation ('nbsp', nbsp_semantics);
+	console.log (sem (cst).nbsp ());
+    } else {
+	var pos = cst._rightmostFailurePosition;
+	console.error (parser.trace (grammar).toString ());
+	console.log ("FAIL: at position " + pos.toString ());
+    }
 }
+
+console.log ("grammar -> str small");
+test (grammar, str_small);
+
+var fstr = fs.readFileSync ('nbsp.txt');
+console.log ("grammar -> str small from file");
+test (grammar,fstr);
+
+
+var fgrammar = fs.readFileSync ('nbsp.ohm');
+var fstr = fs.readFileSync ('nbsp.txt');
+console.log ("fgrammar -> str small");
+test (fgrammar, str_small);
+console.log ("fgrammar -> str small from file");
+test (fgrammar, fstr);
+
