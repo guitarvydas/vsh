84ac5f40092b953ca75c69d6b6efe120e96038e7
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-25 15:55:13 -0400

    fixups " " to "_" ; rename "-" "_"

--- fixup.pl
+++ fixup.pl
@@ -1,6 +1,6 @@
 :- use_module(library(pcre)).
 
 fixupName(Original,Fixed):-
-    re_replace(" "/g,"-",Original,Fixed).
+    re_replace(" "/g,"_",Original,Fixed).
 
     
--- run.bash
+++ run.bash
@@ -4,13 +4,13 @@ names=`node emitBash.js 7.json`
 # echo chmod a+x ${names}
 # chmod a+x ${names}
 
-pfr drawio-to-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/drawio-to-factbase
-pfr create-rect-facts unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/create-rect-facts
-pfr sort-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/sort-factbase
-pfr run-queries unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/run-queries
-pfr emit-bash unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/emit-bash
-pfr permissions-to-execute unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/permissions-to-execute
-pfr make_vsh unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh-boot/make_vsh
+pfr drawio_to_factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/drawio_to_factbase
+pfr create_rect_facts unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/create_rect_facts
+pfr sort_factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/sort_factbase
+pfr run_queries unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/run_queries
+pfr emit_bash unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/emit_bash
+pfr permissions_to_execute unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/permissions_to_execute
+pfr make_vsh unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw >../vsh_boot/make_vsh
 
-chmod a+x ../vsh-boot/drawio-to-factbase ../vsh-boot/create-rect-facts ../vsh-boot/sort-factbase ../vsh-boot/run-queries ../vsh-boot/emit-bash ../vsh-boot/permissions-to-execute
-ls -l ../vsh-boot
+chmod a+x ../vsh_boot/drawio_to_factbase ../vsh_boot/create_rect_facts ../vsh_boot/sort_factbase ../vsh_boot/run_queries ../vsh_boot/emit_bash ../vsh_boot/permissions_to_execute ../vsh_boot/make_vsh
+ls -l ../vsh_boot
