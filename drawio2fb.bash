57c464e1d35ba1f7221f4a85893cba3e85dd1c4e
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-25 08:11:45 -0400

    drawio2fb as pipeline

--- drawio2fb.bash
+++ drawio2fb.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 cdir=`pwd`
-pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js >1.txt
-pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js >2.txt
-pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js >3.txt
-pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js >4.txt
-sort <4.txt >5.pl
+# pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js >1.txt
+# pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js >2.txt
+# pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js >3.txt
+# pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js >4.txt
+# sort <4.txt >5.pl
+
+
+pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js \
+    | pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js \
+    | pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js \
+    | pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js \
+    | sort >5.pl
