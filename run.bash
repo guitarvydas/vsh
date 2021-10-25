812fb3d222ef5f9e13af18770c64b5b59457b6ed
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-25 08:45:38 -0400

    handle div

--- run.bash
+++ run.bash
@@ -4,12 +4,12 @@ names=`node emitBash.js`
 echo chmod a+x ${names}
 chmod a+x ${names}
 
-pfr drawio-to-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
-pfr create-rect-facts unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
-pfr sort-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
-pfr run-queries unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
-pfr emit-bash unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
-pfr permissions-to-execute unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw
+pfr drawio-to-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
+pfr create-rect-facts unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
+pfr sort-factbase unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
+pfr run-queries unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
+pfr emit-bash unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
+pfr permissions-to-execute unhtml.ohm unhtml.srw | pfr - span.ohm span.srw | pfr - para.ohm para.srw | pfr - div.ohm div.srw
 
 # echo
 # echo '*** running VSH0 ***'
