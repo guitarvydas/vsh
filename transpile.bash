de7b2fc4b05680a3350a264ba450ea34ff1db971
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-25 08:13:48 -0400

    cut drawio2fb.bash out, inline in transpile.bash

--- transpile.bash
+++ transpile.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-./drawio2fb.bash
+# ./drawio2fb.bash
+
+cdir=`pwd`
+pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js \
+    | pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js \
+    | pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js \
+    | pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js \
+    | sort >5.pl
+
 
 ## create rect fact for every vertex that is not an edge/ellipse/text
 ## sequence.drawio file contains vertexes, and marks all edge and ellipse (and text)
