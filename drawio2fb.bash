28ad9448504a4b7bb55618e33c352ed14f070124
Paul Tarvydas <guitarvydas@gmail.com>
2021-10-25 08:42:04 -0400

    rename all .action to .srw
    rename to srw

--- drawio2fb.bash
+++ drawio2fb.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 cdir=`pwd`
-# pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js >1.txt
-# pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js >2.txt
-# pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js >3.txt
-# pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js >4.txt
+# pfr vsh.drawio drawio.ohm drawio.srw $cdir/support.js >1.txt
+# pfr 1.txt styleexpander.ohm styleexpander.srw $cdir/support.js >2.txt
+# pfr 2.txt attributeelider.ohm attributeelider.srw $cdir/support.js >3.txt
+# pfr 3.txt emitFactbase.ohm emitFactbase.srw $cdir/support.js >4.txt
 # sort <4.txt >5.pl
 
 
-pfr vsh.drawio drawio.ohm drawio.action $cdir/support.js \
-    | pfr 1.txt styleexpander.ohm styleexpander.action $cdir/support.js \
-    | pfr 2.txt attributeelider.ohm attributeelider.action $cdir/support.js \
-    | pfr 3.txt emitFactbase.ohm emitFactbase.action $cdir/support.js \
+pfr vsh.drawio drawio.ohm drawio.srw $cdir/support.js \
+    | pfr 1.txt styleexpander.ohm styleexpander.srw $cdir/support.js \
+    | pfr 2.txt attributeelider.ohm attributeelider.srw $cdir/support.js \
+    | pfr 3.txt emitFactbase.ohm emitFactbase.srw $cdir/support.js \
     | sort >5.pl
