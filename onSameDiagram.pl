3abc8b9216867e0456bb88e5eb9abaddd2611a8c
Paul Tarvydas <guitarvydas@gmail.com>
2021-11-18 17:00:20 -0500

    transitive containment

--- onSameDiagram.pl
+++ onSameDiagram.pl
@@ -2,3 +2,9 @@ onSameDiagram(A,B):-
     diagramContains(D,A),
     diagramContains(D,B).
     
+diagramContains(D,X):-
+    diagram(D,_),
+    transitivecontains(D,X).
+
+transitivecontains(D,X):-contains(D,X).
+transitivecontains(D,X):-contains(D,Y),contains(Y,X).
